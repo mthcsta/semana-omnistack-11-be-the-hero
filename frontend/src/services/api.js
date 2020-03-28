b9394661a4fbc31196a32cc1b576737c60b506a1
mthcsta <matheuscostakrenn@gmail.com>
2020-03-27 21:31:24 -0300

    edit front api

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: typeof __BASE_URL__ == "string" ? __BASE_URL__ : 'http://localhost:3333',
+    baseURL:  'http://localhost:3333',
 })
 
 export default api
