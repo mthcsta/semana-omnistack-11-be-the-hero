c8e0041c3fd8ebab7a2110c944ccb6cbbe7c3fbb
mthcsta <matheuscostakrenn@gmail.com>
2020-03-27 21:20:16 -0300

    Change baseUrl api frontend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: process.env.URL || 'http://localhost:3333',
+    baseURL: __BASE_URL__ || 'http://localhost:3333',
 })
 
 export default api
