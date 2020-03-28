dc5808fea52429f4a0eff796fe03b52b297998da
mthcsta <matheuscostakrenn@gmail.com>
2020-03-28 01:28:58 -0300

    change front API url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 let baseURL;
 
 if('__BASE_URL__' in window)
-    baseURL = this.__BASE_URL__
+    baseURL = window.__BASE_URL__
 else
     baseURL = 'http://localhost:3333'
 
